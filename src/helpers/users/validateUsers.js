1da52dbc1ca12d273d36461733ec6d07e466c320
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2023-12-23 03:22:46 -0300

    feat: updated user roles file.

--- src/helpers/users/validateUsers.js
+++ src/helpers/users/validateUsers.js
@@ -1,11 +1,13 @@
-const knex = require('../../database/connection');
+const knex = require("../../database/connection");
 
 const queryEmail = async (email) => {
-  const emailValidate = await knex('dateusers').where({ email });
+  const emailValidate = await knex("dateusers").where({ email });
+  return emailValidate[0];
 };
 
 const queryNick = async (nick_name) => {
-  const nickValidate = await knex('dateusers').where({ nick_name });
+  const nickValidate = await knex("dateusers").where({ nick_name });
+  return nickValidate[0];
 };
 
 module.exports = {
