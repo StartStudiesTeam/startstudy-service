cd58d020afdf6e45807b41a6d348e6a11cc1cb4f
BritoRuan <ruan.desenvolvedor@gmail.com>
2023-12-26 20:55:41 -0300

    fix: updated file routes.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/users/user");
 const { middlewareRegisterUser } = require("../middlewares/validateJoi");
+const { authenticationUser } = require("../middlewares/token");
+
 const schemaAuth = require("../schemas/schemaUserAuth");
 const schemaLogin = require("../schemas/schemaLogin");
-const { authenticationUser } = require("../middlewares/token");
-const knex = require("../database/connection");
 
 const route = express();
 
