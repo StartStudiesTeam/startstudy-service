9388bf76592c71a58cc4f8552c868e64ae65a8fa
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-01 22:08:30 -0300

    feat: swagger import.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-require('dotenv/config');
-const express = require('express');
-const route = require('./routes/routes');
+require("dotenv/config");
+const express = require("express");
+const route = require("./routes/routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./documentation/swagger/swagger.json");
 
 const app = express();
 app.use(express.json());
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(route);
 
 app.listen(process.env.PORT);
