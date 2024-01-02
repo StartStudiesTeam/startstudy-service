b281b1067caefd1867e4723ae1c5f69baf494dfd
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-01 22:09:58 -0300

    fix: slight fixes.

--- src/middlewares/token.js
+++ src/middlewares/token.js
@@ -5,7 +5,7 @@ const authenticationUser = async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.status(401).json({ mensagem: "Não autorizado" });
+    return res.status(401).json({ message: "Não autorizado" });
   }
 
   const token = authorization.split(" ")[1];
@@ -13,11 +13,13 @@ const authenticationUser = async (req, res, next) => {
   try {
     const { sub } = jwt.verify(token, process.env.SECRET_KEY);
 
-    req.user = { id: sub };
+    req.user = {
+      id: sub,
+    };
 
     next();
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json({ message: error.message });
   }
 };
 
