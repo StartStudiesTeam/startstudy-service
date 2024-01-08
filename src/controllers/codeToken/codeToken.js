4eb369d458931626fa9f782fb35a7038b84aedbe
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2024-01-07 22:45:38 -0300

    feat: token validation.

---
+++ src/controllers/codeToken/codeToken.js
@@ -0,0 +1,40 @@
+const knex = require("../../database/connection");
+const { queryEmail } = require("../../helpers/users/validateUsers");
+
+const queryValidationToken = async (req, res) => {
+  const { email, codeToken } = req.body;
+  const user = await queryEmail(email);
+  const currentTime = new Date();
+  currentTime.setHours(currentTime.getHours() - 1);
+
+  try {
+    const queryToken = await knex("dateusers")
+      .innerJoin(
+        "token_confirmation",
+        "dateusers.id",
+        "=",
+        "token_confirmation.user_id"
+      )
+      .where({ email, code_token });
+
+    if (!queryToken[0]) {
+      return res.status(404).json({ message: "Token inválido(a)!" });
+    }
+
+    if (queryToken[0].created_at < currentTime) {
+      return res.status(404).json({ message: "Token expirado!" });
+    }
+
+    const token = jwt.sign({ sub: nick.id }, process.env.SECRET_KEY, {
+      expiresIn: "10m",
+    });
+
+    const { password: _, ...userValid } = nick;
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: "Erro interno no servidor" });
+  }
+};
+
+module.exports = queryValidationToken;
