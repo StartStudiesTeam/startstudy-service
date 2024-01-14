dc264b2b14835bf3dfc065af3478e7a9e27c9487
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-13 23:54:05 -0300

    refactor: corrected database queries but needs to add more.

--- src/helpers/users/helpersUsers.js
+++ src/helpers/users/helpersUsers.js
@@ -1,16 +1,16 @@
 const knex = require("../../database/connection");
 
-const queryEmail = async (email) => {
+const mailUserQuery = async (email) => {
   const emailValidate = await knex("dateusers").where({ email });
   return emailValidate[0];
 };
 
-const queryNick = async (nick_name) => {
+const nickUserQuery = async (nick_name) => {
   const nickValidate = await knex("dateusers").where({ nick_name });
   return nickValidate[0];
 };
 
 module.exports = {
-  queryEmail,
-  queryNick,
+  mailUserQuery,
+  nickUserQuery,
 };
