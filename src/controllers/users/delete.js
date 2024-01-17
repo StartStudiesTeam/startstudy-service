3ab2fb726c7cc36d50df61966ca54a63e8917a4a
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2024-01-17 00:30:30 -0300

    feat: user delete controller added.

---
+++ src/controllers/users/delete.js
@@ -0,0 +1,18 @@
+const knex = require("../../database/connection");
+const errorMessages = require("../../helpers/codeMessages/errorMessages");
+const sucessMessages = require("../../helpers/codeMessages/sucessMessages");
+const currentTime = require("../../helpers/helpersData/data");
+
+const deleteUser = async (req, res) => {
+  const { id } = req.user;
+
+  try {
+    await knex("dateusers").update({ deleted_at: currentTime }).where({ id });
+
+    return res.status(200).json({ message: sucessMessages.userDeleted });
+  } catch (error) {
+    return res.status(500).json({ message: errorMessages.InternalServerError });
+  }
+};
+
+module.exports = deleteUser;
