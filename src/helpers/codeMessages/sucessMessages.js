b85e6c064749d78a0f7f61e0e97c741f64cfc7cc
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2024-01-17 00:31:03 -0300

    feat: delete user message added.

--- src/helpers/codeMessages/sucessMessages.js
+++ src/helpers/codeMessages/sucessMessages.js
@@ -1,6 +1,7 @@
 const sucessMessages = {
   passworReset: "Senha redefinida com sucesso!",
   successfullyRegisteredUser: "Usuário registrado com sucesso!",
+  userDeleted: "Usuário deletado com sucesso!",
 };
 
 module.exports = sucessMessages;
