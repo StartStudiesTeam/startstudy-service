3d606b105032e354335b090db0b901bbf6c3ff22
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-17 22:43:02 -0300

    refactor: improving the login function.

--- src/controllers/users/login.js
+++ src/controllers/users/login.js
@@ -9,69 +9,37 @@ const errorMessages = require("../../helpers/codeMessages/errorMessages");
 const loginUser = async (req, res) => {
   const { nick_name, email, password } = req.body;
   try {
-    if (email) {
-      const existingUser = await mailUserQuery(email);
+    const existingUser = email
+      ? await mailUserQuery(email)
+      : await nickUserQuery(nick_name);
 
-      if (!existingUser) {
-        return res.status(404).json({
-          message: errorMessages.invalidEmailOrPassword,
-        });
-      }
-
-      if (existingUser.deleted_at !== null) {
-        return res.status(404).json({
-          message: errorMessages.invalidEmailOrPassword,
-        });
-      }
-
-      const correctPass = await bcrypt.compare(password, existingUser.password);
-
-      if (!correctPass) {
-        return res.status(404).json({
-          message: errorMessages.invalidEmailOrPassword,
-        });
-      }
-
-      const token = jwt.sign({ sub: existingUser.id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
+    if (!existingUser) {
+      return res.status(404).json({
+        message: errorMessages.invalidCredentials,
       });
-
-      const { password: _, ...userValid } = existingUser;
-
-      return res.status(200).json({ token });
     }
 
-    if (nick_name) {
-      const existingNick = await nickUserQuery(nick_name);
-
-      if (!existingNick) {
-        return res.status(404).json({
-          message: errorMessages.invalidNickNameOrPassword,
-        });
-      }
-
-      if (existingNick.deleted_at !== null) {
-        return res.status(404).json({
-          message: errorMessages.invalidNickNameOrPassword,
-        });
-      }
-
-      const correctPass = await bcrypt.compare(password, existingNick.password);
-
-      if (!correctPass) {
-        return res.status(404).json({
-          message: errorMessages.invalidNickNameOrPassword,
-        });
-      }
-
-      const token = jwt.sign({ sub: existingNick.id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
+    if (existingUser.deleted_at !== null) {
+      return res.status(404).json({
+        message: errorMessages.invalidCredentials,
       });
-
-      const { password: _, ...userValid } = existingNick;
-
-      return res.status(200).json({ token });
     }
+
+    const correctPass = await bcrypt.compare(password, existingUser.password);
+
+    if (!correctPass) {
+      return res.status(404).json({
+        message: errorMessages.invalidCredentials,
+      });
+    }
+
+    const token = jwt.sign({ sub: existingUser.id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    const { password: _, ...userValid } = existingUser;
+
+    return res.status(200).json({ token });
   } catch (error) {
     return res.status(500).json({ message: errorMessages.InternalServerError });
   }
