1bafc91d881f23c2b49c02b6dfffc5eb89ff3ba8
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-20 20:06:35 -0300

    feat: added a new message return.

--- src/helpers/codeMessages/errorMessages.js
+++ src/helpers/codeMessages/errorMessages.js
@@ -4,10 +4,12 @@ const errorMessages = {
   invalidEmail: "Email Inválido!",
   passwordInvalid: "Senha Atual Inválida!",
   invalidCredentials: "Credenciais de Acesso Inválidas!",
-  InternalServerError: "Erro Interno no Servidor!",
+  InternalServerError: "Erro Interno do Serviço!",
   existingUser: "Já Existe Usuário Cadastrado Com o E-mail Informado!",
   uniqueNickName: "O Nick Name Precisa Ser Único!",
   unauthorizedUser: "Usuário Não Autorizado!",
+  errorProcessingThisRequest:
+    "Erro ao Processar Esta Solicitação. Tente novamente ou entre em contato com suporte.",
 };
 
 module.exports = errorMessages;
