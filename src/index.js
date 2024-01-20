3de695a0741ee670efe847320165d76d8b436562
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-20 14:22:47 -0300

    refactor: small fixes.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 require("dotenv/config");
 const express = require("express");
 const route = require("./routes/routes");
+
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocs = require("./documentation/swagger/swagger.json");
+const swaggerFile = require("./documentation/swagger/swagger.json");
 
 const app = express();
+
 app.use(express.json());
-app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/api/v1", route);
 
 app.listen(process.env.PORT);
