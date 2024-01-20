08bf274781318e06d6a329a02be0bdd270c20404
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-20 14:26:13 -0300

    feat: added users swagger definitions.

---
+++ src/documentation/exampleDocs/data.js
@@ -0,0 +1,26 @@
+exports.UserSignUp = {
+  id: 1,
+  name: "Samuel",
+  nick_name: "samucajnew",
+  email: "samuel@gmail.com",
+  phone_number: "71945458585",
+  password: "HAXXHASAGHDFYDAFDUISGFU177663###",
+};
+
+exports.UserSingIn = {
+  email: "samuel@gmail.com",
+  password: "HAXXHASAGHDFYDAFDUISGFU177663###",
+};
+
+exports.UserMailCheck = {
+  email: "samuel@gmail.com",
+};
+
+exports.UserConfirmationToken = {
+  email: "samuel@gmail.com",
+  codeToken: "12cdyt",
+};
+
+exports.UserPasswordForget = {
+  newPassword: "@lfvkjkj92847",
+};
