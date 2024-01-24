eb2cea181134b1727ee74ccf68dd956311cd71f4
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-23 23:09:05 -0300

    refactor: mail check controller refactoring.

--- src/controllers/mails/mails.js
+++ src/controllers/mails/mails.js
@@ -1,12 +1,16 @@
+const knex = require("../../database/connection");
 const errorMessages = require("../../helpers/codeMessages/errorMessages");
 const sucessMessages = require("../../helpers/codeMessages/sucessMessages");
-const { mailUserQuery } = require("../../helpers/users/helpersUsers");
+const { currentTime } = require("../../helpers/helpersData/data");
+const { getByMail } = require("../../helpers/users/helpersUsers");
+const crypto = require("crypto");
+const mailSendUserResgistered = require("../mails/sendMails");
 
 const mailCheckQuery = async (req, res) => {
   const { email } = req.body;
 
   try {
-    const mailExistingQuery = await mailUserQuery(email);
+    const mailExistingQuery = await getByMail(email);
 
     if (!mailExistingQuery) {
       return res.status(400).json({
@@ -14,7 +18,24 @@ const mailCheckQuery = async (req, res) => {
       });
     }
 
-    return res.status(204).send();
+    const updatedCode = {
+      code_token: crypto.randomBytes(3).toString("hex"),
+      created_at: currentTime,
+      updated_at: currentTime,
+      confirmation_at: null,
+    };
+
+    mailSendUserResgistered(
+      mailExistingQuery.name,
+      email,
+      updatedCode.code_token
+    );
+
+    await knex("token_confirmation")
+      .update(updatedCode)
+      .where({ id: mailExistingQuery.id });
+
+    return res.status(200).json({ message: sucessMessages.checkMailUser });
   } catch (error) {
     return res
       .status(404)
