507cdaf1a22687e37a7826a1d354fecb1e902197
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-01-23 23:11:44 -0300

    refactor: small corrections, changing the names of some queries and adding them.

--- src/helpers/users/helpersUsers.js
+++ src/helpers/users/helpersUsers.js
@@ -1,17 +1,18 @@
 const knex = require("../../database/connection");
+const { currentTime } = require("../helpersData/data");
 
-const mailUserQuery = async (email) => {
+const getByMail = async (email) => {
   const emailValidate = await knex("dateusers").where({ email });
   return emailValidate[0];
 };
 
-const nickUserQuery = async (nick_name) => {
+const getByNickname = async (nick_name) => {
   const nickValidate = await knex("dateusers").where({ nick_name });
   return nickValidate[0];
 };
 
-const validationCodeTokenDatabaseQuery = async (email, code) => {
-  const tokenQuery = await knex("dateusers")
+const getByMailAndCode = async (email, code) => {
+  const codeToken = await knex("dateusers")
     .innerJoin(
       "token_confirmation",
       "dateusers.id",
@@ -21,12 +22,12 @@ const validationCodeTokenDatabaseQuery = async (email, code) => {
     .where({ email, code_token: code })
     .first();
 
-  console.log(tokenQuery);
+  console.log(codeToken);
 
-  return tokenQuery;
+  return codeToken;
 };
 
-const mailValidLoginQuery = async (userAcess) => {
+const getVerifyMail = async (userAcess) => {
   const loginQuery = await knex("dateusers")
     .select("verify_mail")
     .where({
@@ -39,18 +40,29 @@ const mailValidLoginQuery = async (userAcess) => {
   return loginQuery;
 };
 
-const verifyEmailColumnUpdate = async (email) => {
-  const emailUpdate = await knex("dateusers")
-    .update({ verify_mail: true })
-    .where({ email });
+const updateVerifyMail = async (email) => {
+  await knex.transaction(async (trx) => {
+    const [updatedRow] = await trx("dateusers")
+      .update({ verify_mail: true })
+      .where({ email })
+      .returning("*");
 
-  return emailUpdate;
+    userId = updatedRow.id;
+
+    await trx("token_confirmation")
+      .update({ confirmation_at: currentTime })
+      .where({ user_id: userId });
+
+    await trx.commit();
+  });
+
+  return updateVerifyMail;
 };
 
 module.exports = {
-  mailUserQuery,
-  nickUserQuery,
-  validationCodeTokenDatabaseQuery,
-  mailValidLoginQuery,
-  verifyEmailColumnUpdate,
+  getByMail,
+  getByNickname,
+  getByMailAndCode,
+  getVerifyMail,
+  updateVerifyMail,
 };
