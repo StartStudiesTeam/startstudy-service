e5db2b75ab6bcd2a06ee536b5dd2209d4e890536
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-02-07 00:19:47 -0300

    delete: deleting unused codes.

--- src/helpers/helpersData/data.js
+++
@@ -1,7 +0,0 @@
-const currentTime = new Date();
-
-const expireTime = currentTime.setHours(currentTime.getHours() + 1);
-
-const formattedDate = currentTime.toDateString();
-
-module.exports = { currentTime, formattedDate };
