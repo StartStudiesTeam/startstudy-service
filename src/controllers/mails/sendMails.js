83ef8aadc012b36d1b931ba3d614a73c56fc539c
samuel-jesus <samuel.jesan2018@gmail.com>
2024-02-23 08:30:43 -0300

    fix: substitute template in send mail user registered, and modified variables name

--- src/controllers/mails/sendMails.js
+++ src/controllers/mails/sendMails.js
@@ -2,15 +2,15 @@ const transport = require("../../service/mail/connectionEmail.js");
 const compileHtml = require("../../helpers/sendMail/compile.js");
 
 const mailSendUserResgistered = async (name, email, codeToken) => {
-  const html = await compileHtml("./src/templates/register.html", {
-    name: name,
-    token: codeToken,
+  const html = await compileHtml("./src/templates/registered-user.html", {
+    name,
+    codeToken,
   });
 
   transport.sendMail({
     from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}`,
     to: `${name} <${email}>`,
-    subject: `Solicitação de Cadastro no Start Study`,
+    subject: `Bem vindo(a) ao StartStudies!`,
     html,
   });
 };
