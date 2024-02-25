b2c9f60b11113d54f69ced4461ee56a9082e27b2
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-02-25 17:06:59 -0300

    feat: created controller create roadmap.

---
+++ src/controllers/roadmap/create.js
@@ -0,0 +1,37 @@
+const prisma = require("../../database/prisma");
+const errorMessages = require("../../helpers/codeMessages/errorMessages");
+const sucessMessagesRoadmap = require("../../helpers/codeMessages/roadmapSucessMessages");
+const { findUserMail } = require("../../models/User");
+
+const createdRoadmap = async (req, res) => {
+  const { email, title, description } = req.body;
+
+  const user = await findUserMail(email);
+
+  try {
+    const create = await prisma.roadmap.create({
+      data: {
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+        title,
+        description,
+      },
+    });
+
+    const { updatedAt, deletedAt: _, ...createdRoadmap } = create;
+
+    return res.status(201).json({
+      message: sucessMessagesRoadmap.successfullyRegisteredRoadmap,
+      body: { createdRoadmap },
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: errorMessages.errorProcessingThisRequest });
+  }
+};
+
+module.exports = createdRoadmap;
