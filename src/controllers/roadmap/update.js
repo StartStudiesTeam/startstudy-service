d4408b6a3f4b473490281c9d47b8640e68e98950
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-02-25 17:07:30 -0300

    feat: created controller update roadmap.

---
+++ src/controllers/roadmap/update.js
@@ -0,0 +1,34 @@
+const prisma = require("../../database/prisma");
+const errorMessages = require("../../helpers/codeMessages/errorMessages");
+const sucessMessagesRoadmap = require("../../helpers/codeMessages/roadmapSucessMessages");
+const { currentTime } = require("../../helpers/helpersData/date");
+
+const updateRoadmap = async (req, res) => {
+  const { id, title, description } = req.body;
+
+  try {
+    const roadmap = await prisma.roadmap.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        updatedAt: currentTime,
+      },
+    });
+
+    const { updatedAt, deletedAt: _, ...updatedRoadmap } = roadmap;
+
+    return res.status(200).json({
+      message: sucessMessagesRoadmap.successUpdateRoadmap,
+      body: { updatedRoadmap },
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: errorMessages.errorProcessingThisRequest });
+  }
+};
+
+module.exports = updateRoadmap;
