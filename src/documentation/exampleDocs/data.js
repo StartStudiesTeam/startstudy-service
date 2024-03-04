2ca7e316a2982e32726aeed82a895c9cb664cda0
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-04 05:28:47 -0300

    refactor: updated exports model data.

--- src/documentation/exampleDocs/data.js
+++ src/documentation/exampleDocs/data.js
@@ -1,26 +1,57 @@
 exports.UserSignUp = {
   id: 1,
-  name: "Samuel",
-  nick_name: "samucajnew",
-  email: "samuel@gmail.com",
-  phone_number: "71945458585",
-  password: "HAXXHASAGHDFYDAFDUISGFU177663###",
+  name: "Username",
+  nick_name: "username",
+  email: "user@email.com",
+  phone_number: "XXXXXXXXXXX",
+  password: "user@k-vbgauR@$#",
 };
 
 exports.UserSingIn = {
-  email: "samuel@gmail.com",
-  password: "HAXXHASAGHDFYDAFDUISGFU177663###",
+  email: "user@email.com",
+  password: "user@k-vbgauR@$#",
 };
 
 exports.UserMailCheck = {
-  email: "samuel@gmail.com",
+  email: "user@email.com",
 };
 
 exports.UserConfirmationToken = {
-  email: "samuel@gmail.com",
+  email: "user@email.com",
   codeToken: "12cdyt",
 };
 
 exports.UserPasswordForget = {
+  email: "user@email.com",
   newPassword: "@lfvkjkj92847",
+};
+
+exports.UserRefreshToken = {
+  refresh_token: "9fede3d3-eeb4-48f0-9db0-93ea6157fde6",
+};
+
+exports.CreateRoadmap = {
+  title: "Learn JavaScript",
+  description: "The best tutorial to learn to JavaScript",
+};
+
+exports.UpdateRoadmap = {
+  title: "Learn Java",
+  description: "The best tutorial to learn to Java",
+};
+
+exports.CreateComments = {
+  comments: "Amazing! nice rutorial.",
+};
+
+exports.UpdateComments = {
+  comments: "Amazing! Nice tutorial.",
+};
+
+exports.CreateCommentsComments = {
+  comments: "I agree with you",
+};
+
+exports.UpdateCommentsComments = {
+  comments: "I don't agree with you anymore",
 };
