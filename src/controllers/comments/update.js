02584f9cb949adc61d14839e348685b40a0cc67d
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-05 11:45:10 -0300

    refactor: refactoring of comments controllers

--- src/controllers/comments/update.js
+++ src/controllers/comments/update.js
@@ -1,28 +1,18 @@
-const prisma = require("../../database/prisma");
 const sucessMessagesComments = require("../../helpers/codeMessages/commentsSucessMessages");
 const errorMessages = require("../../helpers/codeMessages/errorMessages");
 const { currentTime } = require("../../helpers/helpersData/date");
+const { upgradeComment } = require("../../models/Comment");
 
 const updateComments = async (req, res) => {
   const { id, comments } = req.body;
 
   try {
-    const comments = await prisma.comments.update({
-      where: {
-        id,
-      },
-      data: {
-        comments,
-        updatedAt: currentTime,
-      },
-    });
-
-    const { deletedAt: _, ...updatedComments } = comments;
+    const comment = await upgradeComment(id, comments, currentTime);
 
     return res.status(200).json({
       statusCode: 200,
       message: sucessMessagesComments.successUpdateComments,
-      body: { updatedComments },
+      body: { comment },
     });
   } catch (error) {
     return res.status(400).json({
