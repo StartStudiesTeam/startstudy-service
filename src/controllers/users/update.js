bc5681c27ff8698787947e931dde4c5c26cc9549
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-07 10:33:27 -0300

    refactor: added model reference user

--- src/controllers/users/update.js
+++ src/controllers/users/update.js
@@ -1,9 +1,11 @@
-const prisma = require("../../database/prisma");
 const sucessMessages = require("../../helpers/codeMessages/sucessMessages");
 const errorMessages = require("../../helpers/codeMessages/errorMessages");
-const { findUserMail, findUserNick } = require("../../models/User");
-const codeToken = require("../../helpers/users/token");
-const mailSendUserResgistered = require("../../service/mail/Mails");
+const {
+  findUserMail,
+  findUserNick,
+  upgradeUser,
+} = require("../../models/User");
+
 const { generateToken } = require("../../helpers/authenticate/generateToken");
 const { currentTime } = require("../../helpers/helpersData/date");
 
@@ -11,61 +13,32 @@ const updateUser = async (req, res) => {
   const { id, name, nick_name, email, phone_number } = req.body;
 
   try {
-    const userEmail = await findUserMail(email);
+    const mailUser = await findUserMail(email);
+    const nickNameUser = await findUserNick(nick_name);
 
-    if (userEmail.id !== id) {
-      mailSendUserResgistered(name, email);
-      return res.status(400).json({
-        message: errorMessages.existingUser,
-      });
-    }
-    const userNick = await findUserNick(nick_name);
+    const isInvalidMailUser = mailUser?.id && mailUser?.id !== id;
+    const isInvalidNickNameUser = nickNameUser?.id && nickNameUser?.id !== id;
 
-    if (userNick.id !== id) {
-      console.log(userNick.id);
-      return res.status(400).json({ message: errorMessages.uniqueNickName });
+    if (isInvalidMailUser || isInvalidNickNameUser) {
+      return res.status(400).json({ message: errorMessages.existingUser });
     }
 
-    const updateUser = await prisma.$transaction(async () => {
-      const updateUser = await prisma.users.update({
-        data: {
-          name,
-          email,
-          nickName: nick_name,
-          phoneNumber: phone_number,
-          updatedAt: currentTime,
-          verifyMail: null,
-        },
-      });
+    const response = await upgradeUser(
+      id,
+      name,
+      email,
+      phone_number,
+      currentTime
+    );
 
-      const userId = updateUser.id;
-
-      await prisma.codeToken.update({
-        data: {
-          Users: {
-            connect: {
-              id: userId,
-            },
-          },
-          codeToken: codeToken.code_token,
-        },
-      });
-      return updateUser;
-    });
-
-    mailSendUserResgistered(name, email, codeToken.code_token);
-
-    const accessToken = await generateToken(updateUser.id);
-
-    const { deletedAt, password: _, ...updatedUser } = updateUser;
+    const accessToken = await generateToken(response.id);
 
     return res.status(200).json({
       statusCode: 200,
       message: sucessMessages.successUpdateUser,
-      body: { accessToken, updatedUser },
+      body: { accessToken, response },
     });
   } catch (error) {
-    console.log(error);
     return res.status(400).json({
       statusCode: 400,
       message: errorMessages.errorProcessingThisRequest,
