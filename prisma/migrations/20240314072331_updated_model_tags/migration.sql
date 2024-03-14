80ad8cb96390bfb446a1ca5a8c676e90df3866bb
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-14 05:46:50 -0300

    fix: small update to prism model.

---
+++ prisma/migrations/20240314072331_updated_model_tags/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `user_id` on the `tags` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tags" DROP COLUMN "user_id";
