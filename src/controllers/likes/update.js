1bac30c6d640b392a1bb220a29e873e76b19066b
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2024-03-20 22:44:33 -0300

    fix: bug fix controllers update likes

--- src/controllers/likes/update.js
+++ src/controllers/likes/update.js
@@ -1,12 +1,26 @@
 const errorMessages = require("../../constants/codeMessages/errorMessages");
 const { currentTime } = require("../../utils/date/date");
-const { UpgradeLike } = require("../../models/Like");
+const {
+  UpgradeLike,
+  GetLike,
+  GetFieldDeleteByLikeId,
+} = require("../../models/Like");
 const sucessMessagesLikes = require("../../constants/codeMessages/sucessMessagesLikes");
 
 const updateLike = async (req, res) => {
   const { id, userId } = req.body;
 
   try {
+    const findLike = await GetLike(id);
+    const likesDelete = await GetFieldDeleteByLikeId(id);
+
+    if (!findLike || !likesDelete) {
+      return res.status(404).json({
+        statusCode: 404,
+        message: errorMessages.errorProcessingThisRequest,
+        body: {},
+      });
+    }
     const likes = await UpgradeLike(id, userId, currentTime);
 
     return res.status(200).json({
