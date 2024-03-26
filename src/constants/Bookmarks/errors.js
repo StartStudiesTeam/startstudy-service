64cc1f0c07f0ffe6c054dc8ce59851f34dbec085
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-25 22:54:27 -0300

    feat: added new messages to bookmarks.

---
+++ src/constants/Bookmarks/errors.js
@@ -0,0 +1,7 @@
+const bookmarksErrors = {
+  errorRegisteringBookmark: "Erro, não foi possível registrar o bookmark!",
+  errorWhenUpdatingBookmark: "Erro, não foi possível atualizar o bookmark!",
+  errorWhenDeletingBookmark: "Erro, não foi possível deletar o bookmark!",
+};
+
+module.exports = bookmarksErrors;
