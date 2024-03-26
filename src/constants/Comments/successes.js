3419921ac7d36457132ea8a112d5fd68b8a7a1b4
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-25 22:54:45 -0300

    feat: added new messages to comments

---
+++ src/constants/Comments/successes.js
@@ -0,0 +1,7 @@
+const commentsSuccesses = {
+  successfulInRegisteringComment: "Comentário registrado com sucesso!",
+  successUpdatingComment: "Comentário atualizado com sucesso!",
+  successfulDeletingComment: "Comentário deletado com sucesso!",
+};
+
+module.exports = commentsSuccesses;
---
+++ src/constants/CommentsComments/successes.js
@@ -0,0 +1,7 @@
+const commentsCommentsSuccesses = {
+  successfulInRegisteringCommentComment: "Comentário registrado com sucesso!",
+  successUpdatingCommentComment: "Comentário atualizado com sucesso!",
+  successfulDeletingCommentComment: "Comentário deletado com sucesso!",
+};
+
+module.exports = commentsCommentsSuccesses;
