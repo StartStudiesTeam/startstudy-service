980c5b6a45259aa775d1bc254bc17c31f1c0eb10
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-26 13:49:06 -0300

    feat: added new roadmap messages.

--- src/controllers/roadmap/delete.js
+++ src/controllers/roadmap/delete.js
@@ -1,5 +1,5 @@
-const errorMessages = require("../../constants/codeMessages/errorMessages");
-const sucessMessagesRoadmap = require("../../constants/codeMessages/roadmapSucessMessages");
+const RoadmapMessageErrors = require("../../constants/Roadmaps/errors");
+const RoadmapMessageSuccesses = require("../../constants/Roadmaps/successes");
 const { GetRoadmapById, DeletedRoadmapById } = require("../../models/Roadmap");
 const { currentTime } = require("../../utils/date/date");
 
@@ -12,22 +12,22 @@ const deleteRoadmap = async (req, res) => {
     if (!findRoadmap) {
       return res.status(404).json({
         statusCode: 404,
-        message: errorMessages.errorProcessingThisRequest,
+        message: RoadmapMessageErrors.errorDeletingRoadmap,
         body: {},
       });
     }
 
-    const excludeRoadmap = await DeletedRoadmapById(id, currentTime);
+    const data = await DeletedRoadmapById(id, currentTime);
 
     return res.status(204).json({
       statusCode: 204,
-      message: sucessMessagesRoadmap.deletedRoadmap,
+      message: RoadmapMessageSuccesses.successfulDeletingRoadmap,
       body: {},
     });
   } catch (error) {
     return res.status(400).json({
       statusCode: 400,
-      message: errorMessages.errorProcessingThisRequest,
+      message: RoadmapMessageErrors.errorDeletingRoadmap,
       body: {},
     });
   }
