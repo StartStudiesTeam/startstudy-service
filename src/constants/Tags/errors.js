7a180aa95d9aee35502a1d11c79858816866349c
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-25 22:56:49 -0300

    feat: added new messages to tags

---
+++ src/constants/Tags/errors.js
@@ -0,0 +1,5 @@
+const tagsErrors = {
+  errorRegisteringTag: "Erro, não foi possível registrar a tag!",
+  errorUpdatingTag: "Erro, não foi possível atualizar a tag!",
+  errorDeletingTag: "Erro, não foi possível deletar a tag!",
+};
---
+++ src/constants/Tags/successes.js
@@ -0,0 +1,7 @@
+const tagsSuccesses = {
+  successfulInRegisteringTag: "Tag registrada com sucesso!",
+  successUpdatingTag: "Tag atualizada com sucesso!",
+  successfulDeletingTag: "Tag deletada com sucesso!",
+};
+
+module.exports = tagsSuccesses;
