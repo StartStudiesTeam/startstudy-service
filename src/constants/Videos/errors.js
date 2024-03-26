be6c91338cfd642971ff2703ff00b8be87d13836
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-25 22:56:57 -0300

    feat: added new messages to videos.

---
+++ src/constants/Videos/errors.js
@@ -0,0 +1,8 @@
+const videosErrors = {
+  successReadVideos: "Erro, não foi possível retornar o vídeo!",
+  errorRegisteringVideo: "Erro, não foi possível registrar o vídeo.",
+  errorUpdatingVideo: "Erro, não foi possível atualizar o vídeo.",
+  errorDeletingVideo: "Erro , não foi possível deletar o vídeo.",
+};
+
+module.exports = videosErrors;
