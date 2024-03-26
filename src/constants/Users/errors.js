f31edcce095fad4879fe5504e8717b2704b271c1
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-25 22:56:40 -0300

    feat: added new messages to users

---
+++ src/constants/Users/errors.js
@@ -0,0 +1,9 @@
+const userErros = {
+  userLoginError: "Erro, não foi possível fazer login!",
+  errorWhenRegisteringUser: "Erro, não foi possível registrar usuário!",
+  errorUpdatingUser: "Erro, não foi possível atualizar usuário!",
+  errorDeletingUser: "Erro, não foi possível deletar usuário!",
+  errorUpdatingPassword: "Erro, não foi possível redefinir senha!",
+};
+
+module.exports = userErros;
---
+++ src/constants/Users/successes.js
@@ -0,0 +1,9 @@
+const userSuccesses = {
+  successfulUserLogin: "Usuário logado com sucesso!",
+  successInRegisteringUser: "Usuário registrado com sucesso!",
+  successfulUpdatingUser: "Usuário atualizado com sucesso!",
+  successInDeletingUser: "Usuário deletado com sucesso!",
+  successfulPasswordReset: "Senha redefinida com sucesso!",
+};
+
+module.exports = userSuccesses;
