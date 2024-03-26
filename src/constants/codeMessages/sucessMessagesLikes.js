0409a6fdb004f91d76929df02750bdfe62015171
BritoRuan <ruan.desenvolvedor@gmail.com>
2024-03-25 22:53:32 -0300

    delete: old messages removed.

--- src/constants/codeMessages/sucessMessagesLikes.js
+++
@@ -1,8 +0,0 @@
-const sucessMessagesLikes = {
-  deletedLikes: "Like deletado com sucesso!",
-  countLikes: "Sucesso ao contabilizar likes",
-  createLikes: "Sucesso ao criar like",
-  updateLike: "Sucesso ao atualizar like",
-};
-
-module.exports = sucessMessagesLikes;
