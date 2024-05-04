4cd4e0da2fd01ef1277d17a32e557636877cd52e
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-05-04 02:11:21 -0300

    fix: updated routes.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,36 +11,39 @@ const CodeToken = require("../schemas/Token");
 
 const registerUser = require("../controllers/users/register");
 const loginUser = require("../controllers/users/login");
-const mailCheck = require("../controllers/mails/mails");
+const updateUser = require("../controllers/users/update");
 const newPassword = require("../controllers/password/newPassword");
+const deleteUser = require("../controllers/users/delete");
+
+const mailCheck = require("../controllers/mails/mails");
 const checkTokenValidity = require("../controllers/codeToken/codeToken");
 const refreshTokenUser = require("../controllers/users/refresh");
+
 const createdRoadmap = require("../controllers/roadmap/create");
+const readRoadmap = require("../controllers/roadmap/read");
 const updateRoadmap = require("../controllers/roadmap/update");
 const deleteRoadmap = require("../controllers/roadmap/delete");
+
 const createVideos = require("../controllers/videos/create");
+const readVideo = require("../controllers/videos/read");
 const updateVideos = require("../controllers/videos/update");
 const deleteVideo = require("../controllers/videos/delete");
+
 const createLike = require("../controllers/likes/create");
 const updateLike = require("../controllers/likes/update");
 const deleteLike = require("../controllers/likes/delete");
+
 const updateComments = require("../controllers/comments/update");
 const deleteComments = require("../controllers/comments/delete");
 const createComments = require("../controllers/comments/create");
-const createCommentsComments = require("../controllers/commentsComments/create");
-const updateCommentsComments = require("../controllers/commentsComments/update");
-const deleteCommentsComments = require("../controllers/commentsComments/delete");
+
 const createBookmark = require("../controllers/bookmarks/create");
 const updateBookmark = require("../controllers/bookmarks/update");
 const deleteBookmark = require("../controllers/bookmarks/delete");
-const updateUser = require("../controllers/users/update");
-const deleteUser = require("../controllers/users/delete");
+
 const createTag = require("../controllers/tags/create");
 const updateTag = require("../controllers/tags/update");
 const deleteTag = require("../controllers/tags/delete");
-const countLike = require("../controllers/likes/count");
-const readVideo = require("../controllers/videos/read");
-const readRoadmap = require("../controllers/roadmap/read");
 
 const route = express();
 
@@ -71,7 +74,6 @@ route.post("/videos", createVideos);
 route.put("/videos", updateVideos);
 route.delete("/videos", deleteVideo);
 
-route.get("/likes", countLike);
 route.post("/likes", createLike);
 route.put("/likes", updateLike);
 route.delete("/likes", deleteLike);
@@ -83,9 +85,5 @@ route.delete("/bookmarks", deleteBookmark);
 route.post("/comments", createComments);
 route.put("/comments", updateComments);
 route.delete("/comments", deleteComments);
-
-route.post("/commentsOfComments", createCommentsComments);
-route.put("/commentsOfComments", updateCommentsComments);
-route.delete("/commentsOfComments", deleteCommentsComments);
 
 module.exports = route;
