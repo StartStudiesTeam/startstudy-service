c8e459b3235f3c96d6038a988101b30d48e4e0b7
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-08-24 15:55:29 -0300

    feat: added new messages to use in roadmap

--- src/constants/Roadmaps/successes.js
+++ src/constants/Roadmaps/successes.js
@@ -1,5 +1,7 @@
 const roadmapsSuccesses = {
   successReadRoadmap: "Roadmap retornado com sucesso!",
+  successReadFilterRoadmap: "Roadmaps retornado com sucesso!",
+  successReadAllRoadmap: "Todos os roadmap retornado com sucesso!",
   successfulInRegisteringRoadmap: "Roadmap registrado com sucesso!",
   successUpdatingRoadmap: "Roadmap atualizado com sucesso!",
   successfulDeletingRoadmap: "Roadmap deletado com sucesso!",
