fca5c8f31a76b0c0afe87ac69120932f6d9f040c
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-09-14 23:05:07 -0300

    chore: updated config to swagger documentation

--- src/documentation/exampleDocs/data.js
+++ src/documentation/exampleDocs/data.js
@@ -1,28 +1,28 @@
 exports.UserSignUp = {
   id: 1,
-  name: "Username",
-  nick_name: "username",
-  email: "user@email.com",
-  phone_number: "XXXXXXXXXXX",
+  name: "Ruan Brito",
+  nick_name: "ruanbrito",
+  email: "ruanbrito@email.com",
+  phone_number: "11912345678",
   password: "user@k-vbgauR@$#",
 };
 
 exports.UserSingIn = {
-  email: "user@email.com",
+  email: "ruanbrito@email.com",
   password: "user@k-vbgauR@$#",
 };
 
 exports.UserMailCheck = {
-  email: "user@email.com",
+  email: "ruanbrito@email.com",
 };
 
 exports.UserConfirmationToken = {
-  email: "user@email.com",
+  email: "ruanbrito@email.com",
   codeToken: "12cdyt",
 };
 
 exports.UserPasswordForget = {
-  email: "user@email.com",
+  email: "ruanbrito@email.com",
   newPassword: "@lfvkjkj92847",
 };
 
@@ -31,27 +31,24 @@ exports.UserRefreshToken = {
 };
 
 exports.CreateRoadmap = {
+  userId: "1",
   title: "Learn JavaScript",
   description: "The best tutorial to learn to JavaScript",
 };
 
 exports.UpdateRoadmap = {
+  userId: "1",
   title: "Learn Java",
   description: "The best tutorial to learn to Java",
 };
 
 exports.CreateComments = {
+  userId: "1",
   comments: "Amazing! nice rutorial.",
+  roadmapId: "ecd4811c-84c1-4c3f-aefa-9a2fdde2ea1d",
 };
 
 exports.UpdateComments = {
+  commentId: "1",
   comments: "Amazing! Nice tutorial.",
-};
-
-exports.CreateCommentsComments = {
-  comments: "I agree with you",
-};
-
-exports.UpdateCommentsComments = {
-  comments: "I don't agree with you anymore",
 };
