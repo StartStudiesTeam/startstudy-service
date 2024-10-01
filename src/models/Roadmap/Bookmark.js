0093607242e269e47beff18bb558f18b6ec451bc
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-09-30 23:09:21 -0300

    refactor: logical and code improvement bookamrk model

--- src/models/Roadmap/Bookmark.js
+++ src/models/Roadmap/Bookmark.js
@@ -1,26 +1,38 @@
 const prisma = require("../../database/prisma");
 
 const GetBookmarkById = async (id) => {
-  const request = await prisma.bookmarks.findFirst({
+  const bookmark = await prisma.bookmarks.findFirst({
     where: {
       id,
       deletedAt: null,
     },
   });
-  return request;
+  return bookmark;
 };
 
 const CreateBookmark = async (userId, roadmapId) => {
-  const request = await prisma.bookmarks.create({
+  const bookmark = await prisma.bookmarks.create({
     data: {
+      userId,
+      roadmapId,
+      bookmarks: true,
+    },
+  });
+
+  const { updatedAt, deletedAt: _, ...response } = bookmark;
+
+  return response;
+};
+
+const CheckUserAndRoadmapBookmarkFields = async (userId, roadmapId) => {
+  const bookmark = await prisma.bookmarks.findFirst({
+    where: {
       userId,
       roadmapId,
     },
   });
 
-  const { updatedAt, deletedAt: _, ...response } = request;
-
-  return response;
+  return bookmark;
 };
 
 const UpdateBookmark = async (id, roadmapId, time) => {
@@ -51,9 +63,26 @@ const DeleteBookmark = async (id, time) => {
 
   return bookmark;
 };
+
+const DeletingMarked = async (id, time, value) => {
+  const bookmark = prisma.bookmarks.update({
+    where: {
+      id,
+    },
+    data: {
+      bookmarks: value,
+      updatedAt: time,
+    },
+  });
+
+  return bookmark;
+};
+
 module.exports = {
   GetBookmarkById,
+  CheckUserAndRoadmapBookmarkFields,
   CreateBookmark,
   UpdateBookmark,
   DeleteBookmark,
+  DeletingMarked,
 };
